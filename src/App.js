6cd33c7fa5ae00aad57cbe108bf97a4fb7ee4b86
biniu <dawid.binkowski93@gmail.com>
2019-10-16 23:36:03 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import { Container, Row, Col } from 'reactstrap';
+import {
+  Row,
+  Col,
+  Navbar,
+  Button,
+  Container
+} from 'react-bootstrap';
 
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-
-// Be sure to include styles at some point, probably during your bootstraping
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './css/sidebar.css'
 
 import { RenderHabits } from './RenderHabits';
 import { RenderHero } from './RenderHero'
@@ -16,12 +18,20 @@ import { RenderProjects } from './RenderProjects';
 import { RenderTasks } from './RenderTasks';
 
 
-function App() {
-  return (
-    <Container fluid>
-       <Row>
-        <Col md="1" className="navigation">
-        </Col>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      condition: false,
+    };
+    this.toggleSideBar = this.toggleSideBar.bind(this)
+  }
+
+
+  oldUI() {
+    return (
+      <Row>
+        <Col md="1" className="navigation"></Col>
         <Col md="11" className="contentContainer">
           <Row>
             <Col md="6" className="content"> <RenderHero /> </Col>
@@ -33,8 +43,64 @@ function App() {
           </Row>
         </Col>
       </Row>
-    </Container >
-  );
+    )
+  }
+
+  toggleSideBar() {
+    this.setState({
+      condition: !this.state.condition,
+    });
+  }
+
+  render() {
+    return (
+      <div id="wrapper" className={(this.state.condition ? 'd-flex' : 'd-flex toggled')} >
+        <div className="bg-light border-right" id="sidebar-wrapper">
+          <div className="sidebar-heading">Start Bootstrap </div>
+          <div className="list-group list-group-flush">
+            <a href="#" className="list-group-item list-group-item-action">Dashboard</a>
+          </div>
+        </div>
+
+        <div id="page-content-wrapper">
+          <Navbar bg="light" expand="lg">
+            <Button onClick={this.toggleSideBar}>Menu</Button>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            {/* <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+            </Navbar.Collapse> */}
+          </Navbar>
+
+          <div class="container-fluid">
+            <Row>
+              <Col md="6" className="content"> <RenderHero /> </Col>
+              <Col md="6" className="content"> <RenderHabits /> </Col>
+            </Row>
+            <Row>
+              <Col md="6" className="content"> <RenderTasks /> </Col>
+              <Col md="6" className="content"> <RenderProjects /> </Col>
+            </Row>
+          </div>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
