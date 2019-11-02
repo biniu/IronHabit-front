657da1cec1b1451f1a20873d2e5d84cc6efc39b4
biniu <dawid.binkowski93@gmail.com>
2019-11-02 01:15:11 +0100

    add routes for basic sites

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Dashboard from './Dashboard/Dashboard'
+import Projects from './Projects/Projects'
+import Tasks from './Tasks/Tasks'
+import Habits from './Habits/Habits'
+import Dailies from './Dailies/Dailies'
 import LoginPage from './LoginPage/login'
 import {
   BrowserRouter as Router,
@@ -17,16 +21,27 @@ class App extends Component {
   render() {
     return (
       <div id="pageContent">
-      <Router>
-        <Route exact path = "/">
-          <LoginPage/>
-        </Route>
-        <Route exact path = "/dashboard">
-          <Dashboard/>
-        </Route>
-        
-      </Router>
-        
+        <Router>
+          <Route exact path="/">
+            <LoginPage />
+          </Route>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/tasks">
+            <Tasks />
+          </Route>
+          <Route exact path="/habits">
+            <Habits />
+          </Route>
+          <Route exact path="/dailies">
+            <Dailies />
+          </Route>
+        </Router>
+
       </div>
     );
   }
---
+++ src/Habits/Habits.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+
+
+import {
+  Navbar,
+  Button,
+} from 'react-bootstrap';
+
+import { RenderHabits } from '../Dashboard/RenderHabits';
+
+class Habits extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      condition: false,
+    };
+    this.toggleSideBar = this.toggleSideBar.bind(this)
+  }
+
+  toggleSideBar() {
+    this.setState({
+      condition: !this.state.condition,
+    });
+  }
+
+  render() {
+    return (
+      <div id="wrapper" className={(this.state.condition ? 'd-flex' : 'd-flex toggled')} >
+        <div className="bg-light border-right" id="sidebar-wrapper">
+          <div className="sidebar-heading">Start Bootstrap </div>
+          <div className="list-group list-group-flush">
+            <a href="#" className="list-group-item list-group-item-action">Dashboard</a>
+          </div>
+        </div>
+
+        <div id="page-content-wrapper">
+          <Navbar bg="light" expand="lg">
+            <Button onClick={this.toggleSideBar}>Menu</Button>
+            <Navbar.Brand href="#home"> Iron Habit </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </Navbar>
+          <div className="container-fluid">
+            <RenderHabits />
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Habits
